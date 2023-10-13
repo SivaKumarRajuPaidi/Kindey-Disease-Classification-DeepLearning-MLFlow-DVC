f17cc16b0ac3c83f9c16a86eb9846430c26c95bd
MrSivakumarraju <sivakumarraju.skr@gmail.com>
2023-10-13 19:45:14 +0530

    Added Requirements

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Kindey-Disease-Classification-DeepLearning-MLFlow-DVC"
+AUTHOR_USER_NAME = "SivaKumarRajuPaidi"
+SRC_REPO = "KidneyDiseaseClassifier"
+AUTHOR_EMAIL = "sivakumarraju.skr@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for KidneyDiseaseClassifier app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
